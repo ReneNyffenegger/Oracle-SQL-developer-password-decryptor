8c001aa12f662debc37ac6d952ad13be8fe99aed
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-08-07 09:16:10 +0200

    V.2 add -v flag

--- Decrypt_V4.java
+++ Decrypt_V4.java
@@ -1,4 +1,6 @@
 // vi: ft=java
+//
+// V.2
 
 import java.security.MessageDigest;
 import java.security.GeneralSecurityException;
@@ -25,7 +27,6 @@ public class Decrypt_V4 {
     Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
     cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decryption_key, "DES"), new IvParameterSpec(iv));
     return cipher.doFinal(encrypted_password);
-
   }
 
   private static byte[] decrypt_v4(
@@ -35,11 +36,18 @@ public class Decrypt_V4 {
   {
 
     byte[] encrypted_password = Base64.getDecoder().decode(encrypted);
-
     byte[] salt = DatatypeConverter.parseHexBinary("051399429372e8ad");
 
  // key = db_system_id + salt
     byte[] key = new byte[db_system_id.length + salt.length];
+
+    if (verbose) {
+      print_byte_array("encrypted_password", encrypted_password);
+      print_byte_array("salt              ", salt              );
+      print_byte_array("key               ", key               );
+    }
+
+
     System.arraycopy(db_system_id, 0, key, 0, db_system_id.length);
     System.arraycopy(salt, 0, key, db_system_id.length, salt.length);
 
@@ -47,6 +55,10 @@ public class Decrypt_V4 {
     java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
     for (int i=0; i<42; i++) {
       key = md.digest(key);
+    }
+
+    if (verbose) {
+      print_byte_array("key (MD5'd)       ", key               );
     }
 
  // secret_key = key [0..7]
@@ -58,14 +70,38 @@ public class Decrypt_V4 {
     System.arraycopy(key, 8, iv, 0, key.length - 8);
 
     return des_cbc_decrypt(encrypted_password, secret_key, iv);
-
   }
 
 
   public static void main(String[] argv) { try {
 
-    byte[] encrypted    = argv[0].getBytes();
-    byte[] db_system_id = argv[1].getBytes();
+    if (argv.length < 2) {
+       System.out.println("arguments: [-v]  encrypted  db.system.id");
+       return;
+    }
+
+    int pos_encrypted;
+    int pos_db_system_id;
+
+    if (argv[0].equals("-v")) {
+
+      if (argv.length < 3) {
+        System.out.println("arguments: [-v]  encrypted  db.system.id");
+        return;
+      }
+      verbose = true;
+      pos_encrypted    = 1;
+      pos_db_system_id = 2;
+      System.out.println("verbose flag was set");
+    }
+    else {
+      pos_encrypted    = 0;
+      pos_db_system_id = 1;
+    }
+
+    byte[] encrypted    = argv[pos_encrypted   ].getBytes();
+    byte[] db_system_id = argv[pos_db_system_id].getBytes();
+
 
     byte[] x = decrypt_v4(encrypted, db_system_id);
 
@@ -77,7 +113,29 @@ public class Decrypt_V4 {
     catch (Exception e) {
       System.out.println(e.toString());
     }
-
   }
 
+  private static boolean verbose = false;
+
+  private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+  private static void print_byte_array(String what, byte[] array) {
+
+    System.out.print(what);
+    System.out.print(": ");
+
+    char[] hex = new char[2];
+    for (int i = 0; i < array.length; i++) {
+
+      int v = array[i] & 0xFF;
+
+      if (i > 0) {
+         System.out.print(",");
+      }
+      hex[0] = HEX_ARRAY[v >>> 4];
+      hex[1] = HEX_ARRAY[v & 0x0F];
+      System.out.print(hex);
+    }
+    System.out.println();
+  }
 }
